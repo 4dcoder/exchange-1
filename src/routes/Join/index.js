8dd6b4e78f96d162343c3ad216f3ed77088ebd70
WilliamCui31 <444738577@qq.com>
2018-08-11 16:19:11 +0800

    样式优化

--- src/routes/Join/index.js
+++ src/routes/Join/index.js
@@ -1,4 +1,4 @@
-import React, { PureComponent, Fragment } from 'react';
+import React, { PureComponent } from 'react';
 import { withRouter } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { Button, Menu, Dropdown, Icon } from 'antd';
@@ -22,7 +22,7 @@ class Join extends PureComponent {
     const { language, localization } = global;
 
     return (
-      <Fragment>
+      <div className={styles.joinWrap}>
         <Link to="/signin">{localization['登录']}</Link>
         <Link to="/signup">{localization['注册']}</Link>
         <Dropdown
@@ -40,7 +40,7 @@ class Join extends PureComponent {
         </Dropdown>
         <Button onClick={() => this.props.history.push('/')}>{localization['返回首页']}</Button>
         <main>{this.props.children}</main>
-      </Fragment>
+      </div>
     );
   }
 }
