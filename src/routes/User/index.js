410e0bd46b941bc5013d47f28c81cc19001de5bc
WilliamCui31 <444738577@qq.com>
2018-08-17 19:05:11 +0800

    语言下拉样式

--- src/routes/User/index.js
+++ src/routes/User/index.js
@@ -21,24 +21,26 @@ class User extends PureComponent {
     const { language, localization } = this.props;
 
     return (
-      <div className={styles.userWrap}>
-        <Link to="/signin">{localization['登录']}</Link>
-        <Link to="/signup">{localization['注册']}</Link>
-        <Dropdown
-          overlay={
-            <Menu onClick={this.handleSwitchLanguage}>
-              <Menu.Item key="zh_CN">{getFlag('zh_CN')}</Menu.Item>
-              <Menu.Item key="en_US">{getFlag('en_US')}</Menu.Item>
-            </Menu>
-          }
-        >
-          <a className={('ant-dropdown-link', styles.language)} href="javascript:;">
-            {getFlag(language)}
-            <Icon type="down" />
-          </a>
-        </Dropdown>
-        <Button onClick={() => this.props.history.push('/')}>{localization['返回首页']}</Button>
-        <main>{this.props.children}</main>
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <Link to="/signin">{localization['登录']}</Link>
+          <Link to="/signup">{localization['注册']}</Link>
+          <Dropdown
+            overlay={
+              <Menu onClick={this.handleSwitchLanguage}>
+                <Menu.Item key="zh_CN">{getFlag('zh_CN')}</Menu.Item>
+                <Menu.Item key="en_US">{getFlag('en_US')}</Menu.Item>
+              </Menu>
+            }
+          >
+            <a className={('ant-dropdown-link', styles.language)} href="javascript:;">
+              {getFlag(language)}
+              <Icon type="down" />
+            </a>
+          </Dropdown>
+          <Button onClick={() => this.props.history.push('/')}>{localization['返回首页']}</Button>
+          <main>{this.props.children}</main>
+        </div>
       </div>
     );
   }
